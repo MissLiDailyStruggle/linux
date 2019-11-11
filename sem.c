16174dac587054ed7b7f8e70e3cb21d856c68e3f
nbsun <nbsun@localhost.localdomain>
2019-11-11 23:19:43 +0800

    信号量实现互斥与同步

---
+++ sem.c
@@ -0,0 +1,64 @@
+//信号量体会同步与互斥
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+int tickets = 100;
+sem_t sem;
+
+void *thr_eat(void *arg)
+{
+  while(1)
+  {
+    sem_wait(&sem);
+    if(tickets > 0)
+    {
+      usleep(1000);
+      printf("i get a ticket:%d\n",tickets--);
+      sem_post(&sem);
+    }
+    else
+    {
+      sem_post(&sem);
+      pthread_exit(NULL);
+    }
+    //sem_wait(&sem);
+    //printf("delicious\n");
+  }
+  return NULL;
+}
+
+void *thr_cook(void *arg)
+{
+  while(1)
+  {
+    printf("cook noodle\n");
+    usleep(1000);
+    sem_post(&sem);
+  }
+  return NULL;
+}
+int main()
+{
+  int ret ;
+  pthread_t tid1,tid2;
+  sem_init(&sem,0,0);
+  ret = pthread_create(&tid1,NULL,thr_eat,NULL);
+  if(ret != 0)
+  {
+    return -1;
+  }
+  ret = pthread_create(&tid2,NULL,thr_cook,NULL);
+  if(ret != 0)
+  {
+    return -1;
+  }
+  pthread_join(tid1,NULL);
+  pthread_join(tid2,NULL);
+  sem_destroy(&sem);
+ return 0;
+}
+
+
